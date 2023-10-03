2be7a2d0578678b78a1565666c917c0d5b1279c8
rondayvoo <rondayvoo911@gmail.com>
2023-10-03 14:50:19 +0800

    Remmove scatter gather

--- HardwareAi/AXIS_test/program.py
+++ HardwareAi/AXIS_test/program.py
@@ -7,13 +7,13 @@ cfd = sys.path[0]
 
 print('Loading overlay...')
 overlay = Overlay(os.path.join(cfd, 'design_1_wrapper.bit'))
-dma = overlay.axi_dma_0
+dma = overlay.adder_dma
 print('Overlay loaded.')
 
 in_array = [1, 2, 3, 4]
 
-in_buffer = allocate(shape=(4,), dtype=np.uint32)
-out_buffer = allocate(shape=(4,), dtype=np.uint32)
+in_buffer = allocate(shape=(4,), dtype=np.int32)
+out_buffer = allocate(shape=(4,), dtype=np.int32)
 
 in_buffer[:] = in_array
 
