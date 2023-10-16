b842ba42fc4b237d59e07f82f470cd073e17d171
pos0414 <gimins1202@gmail.com>
2023-10-16 05:12:36 -0400

    Add env

--- ExternalComms/jobs/RelayServerJobs.py
+++ ExternalComms/jobs/RelayServerJobs.py
@@ -46,19 +46,8 @@ class RelayServerJobs:
                         # DMA stuff
                     print('80 reached')
                     count = WINDOW - REMOVE
-                    var_threshold = 2.4902001983892765e+17
-                    data_ndarray = np.array(packets)
-                    # Put actual data here
-                    data_var = np.var([s[0] ** 2 + s[1] ** 2 + s[2] ** 2 for s in data_ndarray])
- 
-                    mean_threshold = 814047927.2074074 
-                    var_threshold = 2.4902001983892765e+17 
-                    data_ndarray = np.array(data_arr)# Put actual data here 
-                    # Uncomment for mean threshold 
-                    data_mean = np.mean([s[0] ** 2 + s[1] ** 2 + s[2] ** 2 for s in data_ndarray]) # 
-                    if data_mean > mean_threshold: # 
-                        print('sent to ai')
-                        self.dma.send_to_ai_input_2d(packets)
+                    print('sent to ai')
+                    self.dma.send_to_ai_input_2d(packets)
                     
                     # Send to FPGA # pass
                      # Uncomment for var threshold 
